5c2f7d862c56ec42d625c5ca207db64a1ca9daa3
Nad ALp <nicklas1234la@gmail.com>
2013-05-19 23:25:48 +0200

    This is a test of the select statement in the database class

---
+++ test/testdb.php
@@ -0,0 +1,12 @@
+<?php 
+require((__DIR__)."/../config.php");
+require((__DIR__)."/../libs/Database.php");
+
+$db= new Database(DB_TYPE, DB_HOST, DB_NAME, DB_USER, DB_PASS);
+$var= 'select * FROM task WHERE taskID=1';
+$dbarray=array('name'=>'ole''
+		);
+
+$result=$db->select($var,$dbarray);
+echo "$result";
+?>
