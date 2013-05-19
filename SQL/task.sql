8783c0dc2dedcf9017a152dedf50fcc355d7dd05
Christoffer <chris.kjeldgaard@gmail.com>
2013-05-19 16:27:52 +0200

    added task deadline

--- SQL/task.sql
+++ SQL/task.sql
@@ -4,5 +4,6 @@ CREATE TABLE IF NOT EXISTS task (
   description text NOT NULL,
   sprintID int NOT NULL,
   state varchar(10) NOT NULL,
+  deadline DATE DEFAULT NULL,
   PRIMARY KEY(taskID)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
