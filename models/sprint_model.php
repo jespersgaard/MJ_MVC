0ad8373d260a8c73b6fb64df5db2a9868fcfc219
Server <ckjeld10@student.aau.dk>
2013-05-21 16:37:02 +0200

    Fixed dependencies errors

--- models/sprint_model.php
+++ models/sprint_model.php
@@ -9,19 +9,19 @@
  * must have functions:
  * 		loadTasks($data = array()) where $data is sprintID 					======= ONGOING
  * 		create($data = array()) where $data is idGroup, date_from, date_to	======= DONE
- *		loadSprint($data = array()) where $data is idSprint 				======= DONE
+ *		loadSprint($data = array()) where $data is sprintID 				======= DONE
  * 
  * CREATE TABLE IF NOT EXISTS sprint (
-	idSprint	INT NOT NULL AUTO_INCREMENT,
+	sprintID	INT NOT NULL AUTO_INCREMENT,
 	idGroup		INT NOT NULL,
 	date_from	DATE NOT NULL,
 	date_to		DATE NOT NULL,
-	PRIMARY KEY(idsprint)
+	PRIMARY KEY(sprintID)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
  */
 
  //require((__DIR__)."/../config.php");
- require((__DIR__)."/../libs/Database.php");
+ //require((__DIR__)."/../libs/Database.php");
  require((__DIR__)."/../models/task_model.php");
  
  class Sprint_Model
@@ -49,7 +49,7 @@
  					'date_from' => $data['dateFrom'],
  					'date_to' => $data['dateTo']));
  			$this->isLoaded = TRUE;	
- 			$this->IDGroup = $data['idGroup']; $this->dateTo = $data['dateTo']; $this->dateFrom = $data['dateFrom']; $this->ID = $data['idSprint'];
+ 			$this->IDGroup = $data['idGroup']; $this->dateTo = $data['dateTo']; $this->dateFrom = $data['dateFrom'];
  		}
  	}
  	
@@ -57,13 +57,13 @@
  	{
 		if(!$this->isLoaded)
 		{
-			$sql = 'SELECT idGroup, date_from, date_to FROM sprint WHERE idSprint = :idSprint';
-			$where = array('idSprint' => $data['idSprint']);
+			$sql = 'SELECT idGroup, date_from, date_to FROM sprint WHERE sprintID = :sprintID';
+			$where = array('sprintID' => $data['sprintID']);
 			$result = $this->db->select($sql,$where);
 			
 			if($result)
 			{
-				$this->ID = $data['idSprint'];
+				$this->ID = $data['sprintID'];
 				$this->IDGroup = $result[0]['idGroup'];
 				$this->dateFrom = $result[0]['date_from'];
 				$this->dateTo = $result[0]['date_to'];	
@@ -84,7 +84,7 @@
  		 */
  		$sql = 'SELECT taskID ,name, description, state, deadline FROM task WHERE sprintID = :sprintID';
  		$where = array('sprintID' => $data['sprintID']);
- 		
+ 		echo 'this is loadTasks';
  		
  		$result = $this->db->select($sql,$where);
  		
@@ -92,11 +92,11 @@
  	}
  }
 /*===================== TESTING AREA ==============*/
-//$data = array('idGroup' => 1, 'dateFrom' => '2010-12-12', 'dateTo' => '2011-03-10');
-//$newSprint = new Sprint_Model();
-//$newSprint->create($data);
-
-$load = array('idSprint' => 1);
+$data = array('idGroup' => 1, 'dateFrom' => '2010-12-12', 'dateTo' => '2011-03-10');
+$newSprint = new Sprint_Model();
+$newSprint->create($data);
+echo "hello";
+$load = array('sprintID' => 1);
 $anotherSprint = new Sprint_Model();
 $anotherSprint->load($load);
 $anotherSprint->loadTasks($load);
